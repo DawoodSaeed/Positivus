0810081120e2dfc7e418d9f61fd28a535626f702
muhammaddawoodsaeed <dawoodsaeed.work@gmail.com>
2024-10-05 00:54:03 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { HeroComponent } from "./hero/hero.component";
+import { OurServicesComponent } from "./our-services/our-services.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, OurServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
