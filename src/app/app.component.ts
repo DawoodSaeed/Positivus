37b00b9fa519cd1b1edc92fa0d3fba94bc87f20e
muhammaddawoodsaeed <dawoodsaeed.work@gmail.com>
2024-10-05 12:42:50 +0500

    Implement mobile-first Call to Action component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { HeroComponent } from "./hero/hero.component";
 import { OurServicesComponent } from "./our-services/our-services.component";
+import { CallToActionComponent } from "./call-to-action/call-to-action.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, OurServicesComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, OurServicesComponent, CallToActionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
